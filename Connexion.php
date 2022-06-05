b4b850281365a1a10082a13887d795d662546879
MelvinMMM <105869830+MelvinMMM@users.noreply.github.com>
2022-06-06 00:48:08 +0200

    Melvin Connexion

--- Connexion.php
+++ Connexion.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="css/styleconnexion.css">
+        <link rel="stylesheet" type="text/css" href="css/stylecon.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Connexion</title>
     </head>
@@ -21,24 +21,26 @@
                 }
 
             ?>
-
-            <div class="formconnexion">
-                <div class="formconnexion_title">
-                    <p>Pseudo :</p>
-                    <p>Password :</p>
-                </div>
                 <form method="POST" action="verification.php">
-                    <input class="mb-2 mt-2" type="email" name="email" placeholder="Your email" value="<?= isset($_COOKIE['email']) ? $_COOKIE['email'] : '' ?>">
+                    <div class="form-group w-35 me-5 ms-5">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control" name="email" aria-describedby="emailHelp" value="<?= isset($_COOKIE['email']) ? $_COOKIE['email'] : '' ?>">
+                    </div>
                     
-                    <input type="password" name="mdp" placeholder="Your password">
+                    <div class="form-group w-35 me-5 ms-5">
+                        <label for="exampleInputPassword1">Mot de passe</label>
+                        <input type="password" class="form-control" name="mdp">
+                      </div>
+
+                      <button type="submit" style="background-color: #14FFFA;" class="btn mt-3">Se connecter</button>
+
+                    </form>
                     
-                </form>
             </div>
-            <div class="secoco">
-                <input class="w-50 mt-2" type="submit" value="Se connecter">
-            </div>
-            <div class="sign_in">
-            <a href="inscription.php">Sign on</a>
+
+            <div class="sign_in"><p>Appuyez</p>
+            <a href="inscription.php">ici</a>
+            <p>pour vous s'inscrire</p>
             </div>
         </main>
         <?php include('includes/footer.php'); ?>
